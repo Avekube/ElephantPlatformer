dac19f5e18787cf7b7ba1592f6a3aaf5b5ce7de6
Aveline Thompson <Aveline Thompson>
2016-07-16 18:51:40 -0500

    Commitment is scary

---
+++ Elephant Platformer Game/Assets/Scripts/playerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+    Vector2 movement;
+    public int speed;
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 10;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        switch ((int)horizontalInput)
+        {
+            case 1:
+                movement.x = 1;
+                break;
+            case -1:
+                movement.x = -1;
+                break;
+            case 0:
+                movement.x = 0;
+                break;
+        }
+        movement.x *= speed;
+	}
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+}
