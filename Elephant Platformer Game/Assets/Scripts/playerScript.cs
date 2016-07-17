879bfd191be284b5889fe0190b05a38eaf1e30eb
Kyle <unknown25mil@gmail.com>
2016-07-17 14:45:57 -0500

    Added Enemies (Mouse and Monkey)

--- Elephant Platformer Game/Assets/Scripts/playerScript.cs
+++ Elephant Platformer Game/Assets/Scripts/playerScript.cs
@@ -4,25 +4,13 @@ using System.Collections;
 public class playerScript : MonoBehaviour {
 
     Vector2 movement;
-    public float speed;
-    public float jumpSpeed;
-    public int maxJumps;
-    public float airTime;
-    public float verticalForce;
-    private float reverseGravity;
-    private float distanceToGround;
-    private CharacterController controller;
+    public int speed = 10;
+    public int health = 3;
 
 	// Use this for initialization
 	void Start ()
     {
-        speed = 10.0f;
-        jumpSpeed = 20.0f;
-        maxJumps = 2;
-        airTime = 2.0f;
-        verticalForce = 0;
-        reverseGravity = airTime * Physics2D.gravity.y;
-        distanceToGround = GetComponent<Collider2D>().bounds.extents.y;
+        
 	}
 
     // Update is called once per frame
@@ -42,43 +30,30 @@ public class playerScript : MonoBehaviour {
                 break;
         }
         movement.x *= speed;
-
-        if (isGrounded())
-        {
-            maxJumps = 2;
-            verticalForce = 0;
-            reverseGravity = airTime * Physics2D.gravity.y;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && maxJumps > 0)
-        {
-            maxJumps -= 1;
-            verticalForce = jumpSpeed;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && verticalForce != 0)
-        {
-            reverseGravity -= Time.deltaTime;
-            verticalForce += reverseGravity * Time.deltaTime;
-        }
 	}
+
+    void Die()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+            GetHit();
+    }
+
+    void GetHit()
+    {
+        health--;
+        if (health == 0)
+            Die();
+    }
 
     void FixedUpdate()
     {
-        Vector2 horziontalVector = GetComponent<Rigidbody2D>().velocity;
-        horziontalVector.x = movement.x;
-        GetComponent<Rigidbody2D>().velocity = horziontalVector;
-
-        Vector2 verticalForceVector = GetComponent<Rigidbody2D>().velocity;
-        verticalForceVector.y = verticalForce;
-        GetComponent<Rigidbody2D>().AddForce(verticalForceVector);
-
-    }
-
-    private bool isGrounded()
-    {
-        Vector2 origin = GetComponent<BoxCollider2D>().bounds.center;
-        bool temp = Physics2D.Raycast(origin, -Vector2.up, distanceToGround + 0.1f);
-        return temp;
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+        velocity.x = movement.x;
+        GetComponent<Rigidbody2D>().velocity = velocity;
     }
 }
